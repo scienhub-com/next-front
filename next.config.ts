f87374c3f764e60187f24a8b1408771e51309446
Batchor <batchfy@gmail.com>
2025-04-27 19:43:52 -0700

    next.config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    output: "export",
+    trailingSlash: false,
+    pageExtensions: ["ts", "tsx", "mdx"],
 };
 
 export default nextConfig;
